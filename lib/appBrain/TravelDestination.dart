360281c915f83f3a550c713b5841c6e43e282ad1
DulajKavinda98 <kavindadulaj.1998@gmail.com>
2021-10-11 20:23:05 +0530

    save local

--- lib/appBrain/TravelDestination.dart
+++ lib/appBrain/TravelDestination.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:smart_travel_planner/appBrain/UserReview.dart';
-import 'package:smart_travel_planner/util/hoteldata.dart';
 
 class TravelDestination {
   TravelDestination(
